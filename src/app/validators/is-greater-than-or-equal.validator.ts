6eeca6e1681be67364ed4ce5b750ae8d07663f7a
Maxime Gardier <m.gardierpro@gmail.com>
2026-04-20 16:34:49 +0200

    refactor: Improve custom validator

--- src/app/validators/is-greater-than-or-equal.validator.ts
+++ src/app/validators/is-greater-than-or-equal.validator.ts
@@ -21,12 +21,9 @@ export function IsGreaterThanOrEqual(
           const relatedValue = (args.object as Record<string, unknown>)[
             relatedPropertyName
           ];
-          if (value === undefined || relatedValue === undefined) return true;
-          return (
-            typeof value === 'number' &&
-            typeof relatedValue === 'number' &&
-            value >= relatedValue
-          );
+          if (typeof value !== 'number' || typeof relatedValue !== 'number') return true;
+          return ( value >= relatedValue );
+          
         },
         defaultMessage(args: ValidationArguments) {
           return `${args.property} must be greater than or equal to ${args.constraints[0]}`;
