d4c7e1348a5a776302b2f39b36958fe1876d02cc
Maxime Gardier <m.gardierpro@gmail.com>
2026-04-20 14:59:22 +0200

    feat: Add  custom validator isGreaterThanOrEqual

---
+++ src/app/validators/is-greater-than-or-equal.validator.ts
@@ -0,0 +1,37 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
+
+export function IsGreaterThanOrEqual(
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isGreaterThanOrEqual',
+      target: object.constructor,
+      propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: unknown, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints as [string];
+          const relatedValue = (args.object as Record<string, unknown>)[
+            relatedPropertyName
+          ];
+          if (value === undefined || relatedValue === undefined) return true;
+          return (
+            typeof value === 'number' &&
+            typeof relatedValue === 'number' &&
+            value >= relatedValue
+          );
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be greater than or equal to ${args.constraints[0]}`;
+        },
+      },
+    });
+  };
+}
