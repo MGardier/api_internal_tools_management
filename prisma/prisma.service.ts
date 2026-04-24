fd509e853875a88f0f04a78044c89ba352a9f742
Maxime Gardier <m.gardierpro@gmail.com>
2026-04-24 10:05:00 +0200

    refactor: Change prisma service to use config service

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,31 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import type { EnvConfig } from '@app/config/env.schema';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-    const showLog = process.env.SHOW_SQL === 'true';
+  constructor(configService: ConfigService<EnvConfig, true>) {
+    const showLog = configService.get('SHOW_SQL', { infer: true });
     super({
-      log: showLog ?
-      [
-        {
-          emit: 'stdout',
-          level: 'query',
-        },
-        {
-          emit: 'stdout',
-          level: 'error',
-        },
-        {
-          emit: 'stdout',
-          level: 'info',
-        },
-        {
-          emit: 'stdout',
-          level: 'warn',
-        },
-      ] : [],
-    })
+      log: showLog
+        ? [
+            { emit: 'stdout', level: 'query' },
+            { emit: 'stdout', level: 'error' },
+            { emit: 'stdout', level: 'info' },
+            { emit: 'stdout', level: 'warn' },
+          ]
+        : [],
+    });
   }
 
   async onModuleInit() {
